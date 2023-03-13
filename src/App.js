7d6441928f40ed003addb2758a7a2d953686a763
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-13 15:36:14 +0000

    off to another previous branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ReviewId from "./review-id"
 
 function App() {
   return (
     <div className="App">
       <h1>NC-Games</h1>
+      <Routes>
+        <Route path="/:review_id" element={<ReviewId />}/>
+      </Routes>
     </div>
   );
 }
