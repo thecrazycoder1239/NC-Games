09edb2cb8baa30ca14cb9f22b9436f76d668937f
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-15 12:00:31 +0000

    disabled submit if adding comment

--- src/components/review-comment-adder.jsx
+++ src/components/review-comment-adder.jsx
@@ -8,12 +8,14 @@ import { Link } from "react-router-dom";
 export default function ReviewCommentAdder({ review_id, setComments }) {
     const { loggedUser } = useContext(UsernameContext);
     const [ eligibleForComment, setEligibleForComment ] = useState(true);
+    const [ promptToSignIn, setPromptToSignIn] = useState(false);
     const [ commentBody, setCommentBody ] = useState("")
     const [ hasCommentBeenAdded, setHasCommentBeenAdded ] = useState(true) 
 
     useEffect(()=>{
         if(loggedUser.username === "guest user") {
             setEligibleForComment(false);
+            setPromptToSignIn(true);
         }
     }, [])
 
@@ -21,9 +23,11 @@ export default function ReviewCommentAdder({ review_id, setComments }) {
         <form onSubmit={(event) => {
             event.preventDefault()
             setHasCommentBeenAdded(false)
+            setEligibleForComment(false)
             postComment(review_id, loggedUser, commentBody).then(commentFromApi => {
                 setComments((currentComments) => {
                     setHasCommentBeenAdded(true)
+                    setEligibleForComment(true)
                     return [ ...currentComments, commentFromApi.data.comment ]
                 })
             })
@@ -33,7 +37,7 @@ export default function ReviewCommentAdder({ review_id, setComments }) {
             <img className="comment-profile" src={loggedUser.avatar_url} alt="profile picture"/>
             </Link>
             <p className="comment-username">{loggedUser.username}</p>
-            {!eligibleForComment ? <p>please sign in to comment</p> : null}
+            {promptToSignIn ? <p>please sign in to comment</p> : null}
             <label htmlFor="comment-to-add">comment: </label>
             <input required value={commentBody} id="comment-to-add" onChange={(event) => {setCommentBody(event.target.value)}}></input>
             <button type="submit" disabled={!eligibleForComment}>Comment</button>
