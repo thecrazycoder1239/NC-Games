0939c0fdcb034e9663608d7f25fe04bd50f4539c
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-15 19:08:29 +0000

    added category selector

--- src/components/show-reviews.jsx
+++ src/components/show-reviews.jsx
@@ -1,20 +1,50 @@
 import {useState, useEffect} from "react";
-import { getReviews } from "../utils/axiosData";
+import { getReviews, getCategories } from "../utils/axiosData";
 import CircularProgress from "@mui/material/CircularProgress";
 import ReviewCard from "./review-card";
+import { useSearchParams } from 'react-router-dom';
 
 
 
 export default function ShowReviews() {
-    const [reviews, setReviews] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const categoryQuery = searchParams.get('category')
+
+    let boolean = false;
+
+    if(categoryQuery !== undefined) {
+        boolean = true; 
+    }
+
+    const [reviews, setReviews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedCategory, setSelectedCategory] = useState(categoryQuery)
+    const [categories, setCategories] = useState([])
+    const [hasSubmitted, setHasSubmitted] = useState(boolean)
+    const [ categoryDescription, setCategoryDescription ] = useState("")
+    
 
     useEffect(() => {
-        getReviews().then(response => {
+        getReviews(selectedCategory).then(response => {
             setReviews(response.data.reviews)
             setIsLoading(false)
         })
-    },[])
+    },[searchParams])
+
+    useEffect(() => {
+        getCategories().then(response => {
+            setCategories(response.data.categories)
+        })
+    }, [])
+
+    useEffect(() => {
+        categories.map(category => {
+            if(category.slug === selectedCategory) {
+                setCategoryDescription(category.description)
+            }
+        })
+    }, [searchParams, categories])
 
     const returnItem = isLoading ? (
     <div className="progress-container">
@@ -22,9 +52,34 @@ export default function ShowReviews() {
     <CircularProgress size={150} />
     </div>
   ) : ( 
+    <>
+    <form onSubmit={(event)=>{
+        event.preventDefault()
+        const newParams = new URLSearchParams(searchParams);
+        newParams.set("category", selectedCategory)
+        setSearchParams(newParams)
+        setIsLoading(true)
+        setHasSubmitted(true)
+        setCategoryDescription("")
+    }}>
+        <p>category: </p>
+        <select id="category-selector" onChange={(event) => {
+                setSelectedCategory(event.target.value)
+        }}>
+            {!hasSubmitted ? <option key="all" value={undefined}>all reviews</option> : <option key={selectedCategory} value={selectedCategory}>{selectedCategory}</option>}
+            {categories.map(category => {
+                return (
+                    <option key={category.slug} value={category.slug}>{category.slug}</option>
+                )
+            })}
+            {hasSubmitted ? <option key="all" value={undefined}>all reviews</option> : null}
+        </select>
+        <button type="submit">search</button>
+    </form>
+    <p>{categoryDescription}</p>
         <ol className="review-list">
             {
-                reviews.map(review => {
+            reviews.map(review => {
                     return (
                                 <ReviewCard review={review}
                                 key={review.review_id}/>
@@ -32,6 +87,7 @@ export default function ShowReviews() {
                 })
             }
         </ol>
+    </>
     )
     return <>{returnItem}</>
 }
