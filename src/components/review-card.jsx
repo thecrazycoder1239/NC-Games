272363967799e77f67dc6b8fc3935ac92918ccbc
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-15 10:35:29 +0000

    made error message visible

--- src/components/review-card.jsx
+++ src/components/review-card.jsx
@@ -19,13 +19,13 @@ export default function ReviewCard({ review, setReviews, review_id }) {
         <li className="review-card">
             <Link to={`/reviews/${review.review_id}`} className="link">
             <img src={review.review_img_url} alt="review image" className="review-img"/>
-            {err ? <p>{err}</p> : null}
             <p className="review-title">{review.title}</p>
             <p className="review-designer">made by {review.designer}</p>
             </Link>
             <div className="card-footer">
                 <p className="review-votes">votes: {review.votes + userVote}</p>
                 <button onClick={onClick} disabled={userVote !== 0} className="vote-btn">vote</button>
+                {err ? <p className="vote-error">please connect to wifi to vote</p> : null}
             </div>
         </li>
     )
